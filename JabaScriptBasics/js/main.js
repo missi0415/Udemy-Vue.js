179bb4e7b088d3f7c75d1bd8f35e65167709624c
missi0415 <missi0415@gmail.com>
2021-02-25 22:30:23 +0900

    settimeout

---
+++ JabaScriptBasics/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+{
+  
+  let i = 0;
+  function showTime() {
+    console.log(new Date());
+    //指定した時間の後に1回だけ処理を実行する命令
+    setTimeout(showTime, 1000);
+    i++;
+    if (i > 2) {
+      clearTimeout(timoutId);
+      
+    }
+  }
+  showTime();
+
+}
