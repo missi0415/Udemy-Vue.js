ad3b2daa29c691afab49e64fcc0475a3bec5f57b
missi0415 <missi0415@gmail.com>
2021-03-09 20:33:15 +0900

    section4finish

--- Udemy.Vue.js/js/main.js
+++ Udemy.Vue.js/js/main.js
@@ -1,11 +1,16 @@
 // main.js
-//他のところに変数を定義すればプロパティに持っていける
+
 var data =　{
   message: 'こんにちは',
   name: 'よしぴー'
 }
+//コンポーネントの登録
+//つかえない
+Vue.component("compo", {
+  template: '<p>コンポーネント<p>'
+});
+
 var vm = new Vue({
-  el: '#app',
   data: data,
   computed: {
     myData: function(){
@@ -13,5 +18,64 @@ var vm = new Vue({
     }
   }
 })
-  console.log (data === vm.$data);
-  console.log(vm);
+vm.$mount('#app')
+
+//テンプレートプロパティ
+new Vue({
+  data: {
+    name: 'よしぴー'
+  },
+  template: '<h1>こんにちは、{{name}}</h1>'
+}).$mount('#app2')
+
+new Vue({
+  data: {
+    name: 'よしぴー'
+  },
+  render: function(h){
+    console.log(h('h1','こんにちは' + this.name))
+    return h('h1','こんにちは' + this.name);
+    //これをVNode仮想ノード
+    //domをつくったのではなくjsのオブジェクトを作っている
+  },
+}).$mount('#app3')
+
+var dir = document.createElement('div');
+console.log(dir)
+console.dir(dir)
+console.log(document)
+console.dir(document)
+
+new Vue({
+  el: '#app4',
+  data: {
+    name: 'よしぴー'
+  },
+  beforeCreate: function(){
+    console.log('beforeCreate');
+  },
+  created: function(){
+    console.log('Create');
+  },
+  beforeMount: function(){
+    console.log('beforeMount');
+  },
+  mounted: function(){
+    console.log('mounted');
+  },
+  updated: function(){
+    console.log('updated');
+  },
+  beforeDestroy: function(){
+    console.log('beforeDestroy');
+  },
+  destroyed: function(){
+    console.log('destroyed');
+  },
+  methods: {
+    destroy: function(){
+      this.$destroy()
+    }
+  }
+})
+
