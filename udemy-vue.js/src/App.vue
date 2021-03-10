f5cf8b380a72ef0979dfaa442c7457cce3ea627f
missi0415 <missi0415@gmail.com>
2021-03-10 22:09:43 +0900

    104[finish]

---
+++ udemy-vue.js/src/App.vue
@@ -0,0 +1,52 @@
+<!--//App.Vue-->
+<template>
+<!-- テンプレート内はdivで囲む -->
+<div>
+  <!-- v-bind=> :
+       v-on  => @ -->
+  <LikeHeader header-text="hello">
+    <!-- テンプレートタグはdomに反映されな
+    特定の要素をslot毎に別々に反映させたい
+    v-slot:好き名前引数を取る 
+    必ず<template>タグを用いる
+    親<template v-slot:title></template>
+    子<slot name:title"></slot>-->
+    <template v-slot:title>
+      <h2>こんにちは</h2>
+    </template>
+    <template v-slot:number>
+      <p>{{ number }}</p>
+    </template>
+  </LikeHeader>
+  <LikeNumber :totalNumber="number" @my-click="incrementNumber"></LikeNumber>
+  <LikeNumber :total-number="number"></LikeNumber>
+</div>
+</template>
+
+<script>
+import LikeHeader from "./components/LikeHeader.vue"
+  export default {
+    data() {
+      return {
+        number: 14
+      };
+    },
+    components: {
+      LikeHeader
+    },
+    methods: {
+      incrementNumber(value) {
+        this.number = value;
+      }
+    }
+  };
+</script>
+
+<style scoped>
+div {
+  border: 1px solid blue;
+}
+h1 {
+  color: blue;
+}
+</style>
---
+++ udemy-vue.js/src/components/LikeHeader.vue
@@ -0,0 +1,23 @@
+<!--LikeHeager.vue-->
+<template>
+<div>
+  <!-- スロットは複数置くことができる -->
+  <slot name="title"></slot>
+  <hr>
+  <p>いいねの数</p>
+  <slot name="number"></slot>
+  <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+  props: ["headerText"]
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
