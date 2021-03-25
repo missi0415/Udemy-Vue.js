5700f0bdb064a20af1cba911fd69d36a6379c966
missi0415 <missi0415@gmail.com>
2021-03-25 13:59:11 +0900

    111[start]

--- Udemy.Vue.js/js/main.js
+++ Udemy.Vue.js/js/main.js
@@ -1,81 +1,28 @@
 // main.js
-
-var data =　{
-  message: 'こんにちは',
-  name: 'よしぴー'
-}
-//コンポーネントの登録
-//つかえない
-Vue.component("compo", {
-  template: '<p>コンポーネント<p>'
-});
-
-var vm = new Vue({
-  data: data,
-  computed: {
-    myData: function(){
-      return this.$data;
-    }
-  }
-})
-vm.$mount('#app')
-
-//テンプレートプロパティ
-new Vue({
-  data: {
-    name: 'よしぴー'
+//ローカル登録　特定のvueからのみアクセスできる
+var component = {
+  data: function(){
+    return {
+      number: 12
+    }  
   },
-  template: '<h1>こんにちは、{{name}}</h1>'
-}).$mount('#app2')
-
-new Vue({
-  data: {
-    name: 'よしぴー'
-  },
-  render: function(h){
-    console.log(h('h1','こんにちは' + this.name))
-    return h('h1','こんにちは' + this.name);
-    //これをVNode仮想ノード
-    //domをつくったのではなくjsのオブジェクトを作っている
-  },
-}).$mount('#app3')
-
-var dir = document.createElement('div');
-console.log(dir)
-console.dir(dir)
-console.log(document)
-console.dir(document)
-
-new Vue({
-  el: '#app4',
-  data: {
-    name: 'よしぴー'
-  },
-  beforeCreate: function(){
-    console.log('beforeCreate');
-  },
-  created: function(){
-    console.log('Create');
-  },
-  beforeMount: function(){
-    console.log('beforeMount');
-  },
-  mounted: function(){
-    console.log('mounted');
-  },
-  updated: function(){
-    console.log('updated');
-  },
-  beforeDestroy: function(){
-    console.log('beforeDestroy');
-  },
-  destroyed: function(){
-    console.log('destroyed');
-  },
+  template: '<p>いいね({{number}})<button @click="increment">+1</button></p>',
   methods: {
-    destroy: function(){
-      this.$destroy()
-    }
+    increment: function(){
+        this.number += 1;
+    }  
+  }
+}
+
+new Vue({
+  el: '#app',
+  components: {
+    'my-component': component
+    //第一引数自由な名前　：　変数定義したコンポーネントの名前
+    //こうするとappのみが使っている状況になる
   }
 })
 
+new Vue({
+  el: '#app2',
+})
